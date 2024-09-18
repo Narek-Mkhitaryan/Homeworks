985effab88819d084d8db73ddaab9625847d8c1f
Narek-Mkhitaryan <153456109+Narek-Mkhitaryan@users.noreply.github.com>
2024-09-18 23:38:02 +0400

    Add files via upload
    1.1 Data Structures
    Create a list of integers and add 5 elements to it.
    Create a dictionary with string keys and integer values, and add 5 key-value pairs to it.
    Create a queue of integers and add 5 elements to it.
    Create a stack of integers and add 5 elements to it.
    1.2 Operations on Data Structures
    Calculate and print the sum of all elements in the list.
    Calculate and print the sum of all values in the dictionary.
    Remove 2 elements from the queue and print the remaining elements.
    Remove 2 elements from the stack and print the remaining elements.
    1.3 Additional Tasks
    Print all elements of the list.
    From a three-digit number (e.g., 124), print the largest digit.
    From a three-digit number (e.g., 124), print the smallest digit.
    From an array, find all even values and print their sum.
    Calculate and print the average of all elements in the array.

---
+++ Homework 1/Algorithms_Homework_1.3 (Additional Tasks)/Algorithms_Homework_1.3 (Additional Tasks)/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Algorithms_Homework_1._3__Additional_Tasks_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+//--------------------------------------------------------------------------------------------------
+//Print all elements of the list.
+            List<int> list = new List<int>() {1, 2, 3, 4, 5};
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+
+//--------------------------------------------------------------------------------------------------
+//From a three-digit number (e.g., 124), print the largest digit.
+
+            int number  = 124;
+            int digit1 = number / 100;
+            int digit2 = (number / 10) % 10;
+            int digit3 = number % 10;
+
+            int largestDigit = Math.Max(digit1, Math.Max(digit2, digit3));
+            Console.WriteLine(largestDigit);
+
+//--------------------------------------------------------------------------------------------------
+//From a three - digit number(e.g., 124), print the smallest digit.
+
+            int SmallestDigit = Math.Min(digit1, Math.Min(digit2,digit3));
+            Console.WriteLine(SmallestDigit);
+
+
+//--------------------------------------------------------------------------------------------------
+//From an array, find all even values and print their sum.
+
+            int[] arr = new int[6];
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            arr[3] = 4;
+            arr[4] = 5;
+            arr[5] = 6;
+
+            foreach (var i in arr)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+//--------------------------------------------------------------------------------------------------
+//Calculate and print the average of all elements in the array.
+
+            int Sum = 0;
+            int Step = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Sum += arr[i];
+                Step++;
+            }
+
+            double average = (double)Sum / Step; 
+            Console.WriteLine($"Average: {average}");
+
+        }
+    }
+}
